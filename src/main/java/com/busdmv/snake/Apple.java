786c8ca78a1f0ffe64d41c36a00c7bd52c6e08e5
Busygin Dmitry <busdmv@gmail.com>
2015-10-27 20:36:20 +0300

    My initial commit message

---
+++ src/main/java/com/busdmv/snake/Apple.java
@@ -0,0 +1,24 @@
+package com.busdmv.snake;
+
+public class Apple {
+
+    private int posX;
+    private int posY;
+
+    public Apple() {
+        setRandomPosition();
+    }
+
+    public void setRandomPosition() {
+        posX = (int) (Math.random() * SnakeGame.WIDTH);
+        posY = (int) (Math.random() * SnakeGame.HEIGHT);
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+}
